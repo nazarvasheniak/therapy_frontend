c06e5b5aaff9892e65b99d4acfb0bfb678fece26
Nazar Vasheniak <nazar.vasheniak@gmail.com>
2020-05-10 15:04:50 +0300

    fix video reviews

--- src/app/main/models/review.model.ts
+++ src/app/main/models/review.model.ts
@@ -1,8 +1,8 @@
-import { SafeResourceUrl } from '@angular/platform-browser';
+import { SafeResourceUrl, SafeHtml } from '@angular/platform-browser';
 
 export class Review {
     public image: string;
     public name: string;
     public text: string;
-    public videoLink: string;
+    public videoLink: SafeHtml;
 }
