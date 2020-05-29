4dd49014bf4a51d6f04c977fbbff4fd41cca805d
Nazar Vasheniak <nazar.vasheniak@gmail.com>
2020-05-29 22:28:22 +0300

    video reviws

--- src/app/main/components/video-reviews/video-reviews.component.ts
+++ src/app/main/components/video-reviews/video-reviews.component.ts
@@ -1,15 +1,48 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { SwiperDirective } from 'ngx-swiper-wrapper';
 
 @Component({
     selector: 'video-reviews',
     templateUrl: './video-reviews.component.html',
     styleUrls: ['./video-reviews.component.scss']
 })
-export class VideoReviewsComponent {
+export class VideoReviewsComponent implements AfterViewInit {
 
-    currentSlide = 0;
+    slidesCount: number;
+    currentSlide: number = 0;
 
     constructor() {
+        
+    }
+
+    ngAfterViewInit() {
+        this.slidesCount = document.querySelector(".swiper-wrapper").childElementCount;
+
+        document.querySelector(".swiper-button-prev").addEventListener('click', () => {
+            this.prevSlide();
+        });
+
+        document.querySelector(".swiper-button-next").addEventListener('click', () => {
+            this.nextSlide();
+        });
+    }
+
+    nextSlide() {
+        if (this.currentSlide == (this.slidesCount - 1)) {
+            this.currentSlide = 0;
+
+            return;
+        }
+
+        this.currentSlide++;
+    }
+
+    prevSlide() {
+        if (this.currentSlide == 0) {
+            this.currentSlide = this.slidesCount - 1;
+
+            return;
+        }
+
+        this.currentSlide--;
     }
 }
