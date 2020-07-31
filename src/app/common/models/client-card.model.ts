56ecbcd4f59453db20f278df4eeff95304be17db
Nazar Vasheniak <nazar.vasheniak@gmail.com>
2020-07-31 18:57:12 +0300

    client fix

--- src/app/common/models/client-card.model.ts
+++ src/app/common/models/client-card.model.ts
@@ -1,10 +1,11 @@
 import { User } from './user.model';
 import { Session } from './session.model';
+import { SpecialistSession } from './specialist-session.model';
 
 export class ClientCard {
     public id: number;
     public user: User;
-    public sessions: Session[];
+    public sessions: SpecialistSession[];
     public problemsCount: number;
     public paid: number;
     public refundsCount: number;
