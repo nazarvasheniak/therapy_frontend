37cf8167bbebaf13ffb4d1af3e11610b42af9ee0
Nazar Vasheniak <nazar.vasheniak@gmail.com>
2020-08-02 11:30:07 +0300

    Update loader-interceptor.service.ts

--- src/app/common/interceptors/loader-interceptor.service.ts
+++ src/app/common/interceptors/loader-interceptor.service.ts
@@ -44,7 +44,7 @@ export class LoaderInterceptor implements HttpInterceptor {
 						}
 					},
 					err => {
-						console.log('error' + err);
+						console.log('error', err);
 						this.removeRequest(req);
 						observer.error(err);
 					},
