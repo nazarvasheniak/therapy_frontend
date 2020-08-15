f20c97f46b5b2f33d6fcb68828e4b0a36d82a417
lexie-420 <65898754+lexie-420@users.noreply.github.com>
2020-08-16 02:06:24 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterExtService } from './common/services';
+import { Router, NavigationEnd, ActivatedRoute, ActivationEnd } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
@@ -9,12 +9,35 @@ import { RouterExtService } from './common/services';
 export class AppComponent {
 	title = 'therapy-frontend';
 
-	constructor(private routerService: RouterExtService) {
+	constructor(
+		private route: ActivatedRoute,
+		private router: Router
+	) {
+		console.log(this.route)
+
+		this.router.events.subscribe(event => {
+			if (event instanceof ActivationEnd) {
+				window.scroll(0, 0);
+			}
+			
+			// filter `NavigationEnd` events
+			if (event instanceof NavigationEnd) {
+
+				// get current route without leading slash `/`
+				const eventUrl = /(?<=\/).+/.exec(event.urlAfterRedirects);
+				const currentRoute = (eventUrl || []).join('');
+				
+				// set bgClass property with the value of the current route
+				if (!currentRoute.includes('/sign-in') && !currentRoute.includes('/sign-up')) {
+					document.body.style.background = "#F0F6F3";
+				}
+			}
+		});
 	}
 
 	onActivate(event) {
-		window.scroll(0,0);
+		//window.scroll(0, 0);
 		//or document.body.scrollTop = 0;
-		//or document.querySelector('body').scrollTo(0,0)
+		//or document.querySelector('body').scrollTo(0,0);
 	}
 }
